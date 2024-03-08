fbfb8a6f90221ccbc03f9d75aed917f5659c215f
sandmannmax <sandmannmaxxer@gmail.com>
2024-03-08 21:46:33 +0100

    added mxs keymap

---
+++ keyboards/sofle/keymaps/mxs/config.h
@@ -0,0 +1,17 @@
+#pragma once
+#define SPLIT_USB_DETECT // needed because slave half wont work without it
+
+// Vial Support
+#define VIAL_KEYBOARD_UID \
+    { 0x05, 0xCD, 0x9F, 0x8A, 0xF4, 0xDF, 0xDE, 0xB2 }
+
+// The four corners
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 5, 3, 8 }
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 0, 0, 0, 0 }
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
